28e00149dd331bf18690c5efd002ac7c1a4cbac6
pan <94183456+runtimeee@users.noreply.github.com>
2022-05-18 20:02:52 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nvim
 my nvim configuration
+
+## reference 
+  - [theniceboy/nvim](https://github.com/theniceboy/nvim)
+  - [ravenxrz/dotfiles](https://github.com/ravenxrz/dotfiles)
--- init.sh
+++ init.sh
@@ -5,17 +5,26 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs  https://raw.githubuser
 
 # install 
 yum install -y python3
-pip3 install pynvim
+
 
 # install nodejs,yarn, build coc.nvim
 yum install -y nodejs npm 
 yum install -y neovim python36-neovim
 
-curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+pip3 install pynvim
+npm install -g yarn 
 cd ~/.vim/plugged/coc.nvim && yarn install 
 
-#install nerd-fonts 
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
+
+#install nerd-fonts 
+# download from https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/SourceCodePro
 fc-cache -vf ~/.local/share/fonts
+
+
+# install ranger 
+# https://github.com/ranger/ranger
+
+# install repgrep
+
 
